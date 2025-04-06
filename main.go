27fcf98bfa28d40f1634547fb098a3de7b81d138
Neil Cowie <thecowster@hotmail.co.uk>
2025-04-06 16:15:48 +0100

    Hands-on exercise #58 - basic funcs

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 /*
-TODO
-*/
+Hands-on exercise #58 - basic funcs
+● Hands on exercise
 
+	○ create a func with the identifier foo that returns an int
+	○ create a func with the identifier bar that returns an int and a string
+	○ call both funcs
+
+○ print out their results
+*/
 package main
+
+import "fmt"
+
+func foo() int {
+	return 42
+}
+
+func bar() (int, string) {
+	return 43, "Hello, World!"
+}
 
 func main() {
 
+	x := foo()
+	fmt.Println(x)
+
+	i, s := bar()
+	fmt.Println(i, s)
 }
