0c6750b994433815fe4db7d04202fa2aa388fd5e
Neil Cowie <thecowster@hotmail.co.uk>
2025-04-11 08:12:42 +0100

    Hands-on exercise #64 - tests in go #2

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 /*
-Hands-on exercise #63 - tests in go #1
+Hands-on exercise #64 - tests in go #2 - unit tests
+
+create a unit test for these three functions
 */
 
 package main
 
 import "fmt"
 
-func Add(a int, b int) int {
+func main() {
+	fmt.Printf("%T\n", add)
+	fmt.Printf("%T\n", subtract)
+	fmt.Printf("%T\n", doMath)
+	x := doMath(42, 16, add)
+	fmt.Println(x)
+	y := doMath(42, 16, subtract)
+	fmt.Println(y)
+}
+
+func doMath(a int, b int, f func(int, int) int) int {
+	return f(a, b)
+}
+
+func add(a int, b int) int {
 	return a + b
 }
 
-func main() {
-	fmt.Println(Add(3, 4))
+func subtract(a int, b int) int {
+	return a - b
 }
--- main_test.go
+++ main_test.go
@@ -2,9 +2,34 @@ package main
 
 import "testing"
 
+func TestDoMathAdd(t *testing.T) {
+	actual := doMath(5, 5, add)
+	expected := 10
+	if actual != expected {
+		t.Errorf("Sum was incorrect, got: %v, want: %v.", actual, 10)
+	}
+}
+
 func TestAdd(t *testing.T) {
-	total := Add(5, 5)
-	if total != 10 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	actual := add(9, 3)
+	expected := 12
+	if actual != expected {
+		t.Errorf("Sum was incorrect, got: %v, want: %v.", actual, 10)
+	}
+}
+
+func TestDoMathSubtract(t *testing.T) {
+	actual := doMath(15, 5, subtract)
+	expected := 10
+	if actual != expected {
+		t.Errorf("Sum was incorrect, got: %v, want: %v.", actual, 10)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	actual := subtract(9, 6)
+	expected := 3
+	if actual != expected {
+		t.Errorf("Sum was incorrect, got: %v, want: %v.", actual, 10)
 	}
 }
