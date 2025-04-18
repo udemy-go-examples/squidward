1ecd7fc06f0aa0016734d542e5b05f2bb0da2d17
Neil Cowie <thecowster@hotmail.co.uk>
2025-04-18 12:32:11 +0100

    Hands-on exercise #69 - func expression

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 /*
-Hands-on exercise #68 - anonymous func
+Hands-on exercise #69 - func expression
 
-	● Build and use an anonymous func
-	● anonymous func / func literal / lambda func
-
-An anonymous func, aka a function literal or a lambda function, is a way to define a function
-without giving it a name. Instead, you can directly declare and define a function inline wherever
-it is needed. Anonymous funcs are commonly used when you want to pass a function as an
-argument to another function or when you need to define a short-lived function for a specific
-task.
+	● Assign a func to a variable, then call that func
 */
 package main
 
@@ -16,10 +9,9 @@ import "fmt"
 
 func main() {
 
-	xi := 5
-	func(mx int) {
-		for i := 0; i < mx; i++ {
-			fmt.Println(i)
-		}
-	}(xi)
+	xf := func(value int) string {
+		return fmt.Sprintf("Anonymous function run with value %v", value)
+	}
+
+	fmt.Println(xf(69))
 }
